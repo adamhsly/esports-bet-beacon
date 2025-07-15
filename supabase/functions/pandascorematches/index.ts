01b43616b0d6715667f4276c5afb2375948a6c4b
adamhsly <adamhsly@googlemail.com>
2025-07-15 07:49:05 +0100

    Update index.ts

--- supabase/functions/pandascorematches/index.ts
+++ supabase/functions/pandascorematches/index.ts
@@ -12,7 +12,7 @@ serve(async () => {
   )
 
   const PANDA_API_TOKEN = Deno.env.get('PANDA_SCORE_API_KEY')
-  const BASE_URL = 'https://api.pandascore.co/ow/matches
+  const BASE_URL = 'https://api.pandascore.co/matches/upcoming'
   const PER_PAGE = 50
 
   // Team player cache
